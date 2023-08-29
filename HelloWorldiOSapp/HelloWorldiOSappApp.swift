45f9809e0ab66a05abd3684a7627ac6d5a936424
morgado <aamorgadoc@gmail.com>
2023-08-29 12:56:39 -0600

    Initial Commit

---
+++ HelloWorldiOSapp/HelloWorldiOSappApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldiOSappApp.swift
+//  HelloWorldiOSapp
+//
+//  Created by Angel Morgado on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldiOSappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
