f5107c1583b055003512ad49fd64db61e9b94cf9
morgado <aamorgadoc@gmail.com>
2023-08-29 14:27:07 -0600

    texto gradiente

--- HelloWorldiOSapp/ContentView.swift
+++ HelloWorldiOSapp/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Dragoz")
+                .font(.system(size: 80))
+                .fontWeight(.bold)
+                .foregroundStyle(.teal.gradient)
         }
         .padding()
     }
